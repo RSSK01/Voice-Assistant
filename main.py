a4b0ad6ecd14d740fdb0a87d68e5a2dd4456fdc1
RSSK01 <60211705+RSSK01@users.noreply.github.com>
2020-03-07 23:47:05 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,148 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import os
+import time
+import pyttsx3
+import speech_recognition as sr
+import pytz
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+MONTHS = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+DAYS = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturnday", "sunday"]
+DAY_EXTENTIONS = ["rd", "th", "st", "nd"]
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWain()
+
+def get_audio():
+    r = sr.Rcognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ""
+
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+        except Exception as e:
+            print("Exception: " + str(e))
+    return said
+
+def authenticate_google():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    return service
+
+def get_event(day, service):
+    # Call the Calendar API
+    date = datetime.datetime.combine(day, datetime.datetime.datetime.min.time())
+    end_date = datetime.datetime.combine(date, datetime.datetime.datetime.min.time())
+    utc = pytz.UTC
+    date = datetime.astimezone(utc)
+    end_date = end_date.astimezone(utc)
+    events_result = service.events().list(calendarId='primary', timeMin=now, timeMax=end_date.isformat(),
+                                        singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    else:
+        speak("You have {len(events)} events on this day.")
+
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(start, event['summary'])
+            start_time = str(start.split("T"[1]).split("-"[0]))
+            if int(start_time.split(":")[0]) < 12:
+                start_time = start_time + "am"
+            else:
+                start_time = str(int(start_time.split(":")[0])-12)
+                start_time = start_time + "pm"
+
+            speak(event["summary"] + " at " + start_time)
+
+def get_date(text):
+    text = text.lower()
+    today = datetime.date.today()
+
+    if text.count("today") > 0:
+        return today
+
+    day = -1
+    day_of_weak = -1
+    month = -1
+    year = today.year
+
+    for word in text.split():
+        if word in MONTHS:
+            month = MONTHS.index(word) + 1
+        elif word in DAYS:
+            day_of_weak = DAYS.index(word)
+        elif word.isdigit():
+            day = int(word)
+        else:
+            for ext in DAY_EXTENTIONS:
+                found = word.find(ext)
+                if found > 0:
+                    try:
+                        day = int(word[:found])
+                    except:
+                        pass
+
+    if month < today.month and month != -1:
+        year += 1
+    if day < today.day and month == -1 and day != 1:
+        month += 1
+    if month == -1 and day == -1 and day_of_weak != -1:
+        current_day_of_weak = today.weekday()
+        dif = day_of_weak - current_day_of_weak
+
+        if dif < 0:
+            dif += 7
+            if text.count("next") >= 1:
+                dif += 7
+        return today + datetime.timedelta(dif)
+    if month == -1 or day == -1:
+        return None
+
+    return datetime.date(month=month, day=day, year=year)
+
+SERVICE = authenticate_google()
+print("Start speaking")
+text = get_audio()
+
+CALENDER_STRS = ["what do i have", "do i have plans", "am i busy"]
+for phrase in CALENDER_STRS:
+    if phrase in text.lower():
+        date = get_date(text)
+        if date:
+            get_event(date, SERVICE)
+        else:
+            speak("please try again")
